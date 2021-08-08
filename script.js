06a02a1796f900ffa32f0a3a0704d9cf5f45e1f4
ConalG <conal-glynnmartin@hotmail.com>
2021-08-08 14:03:28 +0100

    Styling fixes
    Fix CSS issues

--- script.js
+++ script.js
@@ -1,43 +1,53 @@
 //Selecting Buttons
-const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operation]');
-const equalButton = document.querySelector('[data-equals]');
-const deleteButton = document.querySelector('[data-delete]');
-const allClearButton = document.querySelector('[data-all-clear]');
-const previousOperandTextElement = document.querySelector('[data-previous-operand]');
-const currentOperandTextElement = document.querySelector('[data-current-operand]');
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
 //Creating Calculator class
 class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement){
-        this.previousOperandTextElement = previousOperandTextElement;
-        this.currentOperandTextElement = currentOperandTextElement;
-        this.clear();
-    };
-};
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+}
 // Clear operation
-clear(){
-    this.currentOperand = '';
-    this.previousOperand = '';
-    this.operation = undefined;
-};
+clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+}
 
 delete(){
 
-};
+}
 
 appendNumber(number){
-
-};
+    if (number === '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand.toString() + number.toString()
+}
 
 chooseOperation(operation){
 
-};
+}
 
 compute(){
 
-};
+}
 
 updateDisplay(){
+    this.currentOperandTextElement.innerText = this.currentOperand;
+}
 
-};
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
